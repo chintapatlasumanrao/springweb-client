6805ac2abc46859b1196b8a36d74c4411a01cddb
chintapatlasumanrao <91577391+chintapatlasumanrao@users.noreply.github.com>
2023-02-20 12:57:32 -0700

    SBWC-1 initial project

---
+++ src/main/java/com/springweb/client/springwebclient/SpringwebClientApplication.java
@@ -0,0 +1,13 @@
+package com.springweb.client.springwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springweb/client/springwebclient/SpringwebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springweb.client.springwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
